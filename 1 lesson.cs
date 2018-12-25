acb7c9c82bee382578f7d786766d56cd292849b2
Iliya-hacker <45923437+Iliya-hacker@users.noreply.github.com>
2018-12-26 00:56:59 +0200

    Add files via upload

---
+++ 1 lesson.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 10;
+            double d = 89.453;
+            float f = 7.7f;
+            short s = 128;
+            double d2 = d + f;
+            i = s * 100;
+            Console.WriteLine(d2 + i);
+
+            
+        }
+    }
+}
